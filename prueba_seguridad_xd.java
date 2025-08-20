7c2669c03cbbc6caf5f41db03591938a6418773e
Ilulu00 <florezsantiago773@gmail.com>
2025-08-19 20:43:34 -0500

    Esto es una prueba para verificar la seguridad

---
+++ prueba_seguridad_xd.java
@@ -0,0 +1,10 @@
+public class prueba_seguridad_xd{
+    public static void main(String[] args) {
+        int num1= 0, num2= 0, sum;
+
+        
+        sum= num1 + num2;
+        System.out.println(sum);
+    }
+
+}
